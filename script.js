d86b02605f52eeb3cf8ebc04bc641b2cb6de0287
Gaurav Tyagi <118004334+Gaurav-Tyagi-cse@users.noreply.github.com>
2025-02-03 20:11:20 +0530

    text

--- script.js
+++ script.js
@@ -61,3 +61,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+
+// 
+
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+    return new bootstrap.Dropdown(dropdownToggleEl)
+})
+
